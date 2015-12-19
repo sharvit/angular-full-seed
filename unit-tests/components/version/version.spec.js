157ecd38f0c36cae6b155039071f3d66b099fd17
Avi Sharvit <sharvita@gmail.com>
2015-12-19 17:30:04 +0200

    make test pass

--- unit-tests/components/version/version.spec.js
+++ unit-tests/components/version/version.spec.js
@@ -7,7 +7,7 @@
 
     describe('version service', function() {
       it('should return current version', inject(function(version) {
-        expect(version).toEqual('0.0.0');
+        expect(version).toEqual('0.0.1');
       }));
     });
   });
