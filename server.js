09b0c28edf0b35fdb57606f77dd0fb6d4b3b5959
Avi Sharvit <sharvita@gmail.com>
2015-12-24 10:40:21 +0200

    add consts

--- server.js
+++ server.js
@@ -3,18 +3,26 @@
   'use strict';
 
   var express = require('express');
+  var path = require('path');
+
+  var PORT          = process.env.PORT || 3000;
+  
+  var RELEASE_PATH  = path.resolve(__dirname,       'build/release' );
+  var INDEX_FILE    = path.resolve(RELEASE_PATH,    'index.html'    );
+
+
   var server = express();
 
   // user express to serve the static release files
-  server.use(express.static(__dirname + '/build/release'));
+  server.use(express.static(RELEASE_PATH));
 
   // html5mode
   server.get('/*', function(req, res) {
     // Use res.sendFile, as it streams instead of reading the file into memory.
-    res.sendFile(__dirname + '/build/release/index.html');
+    res.sendFile(INDEX_FILE);
   });
 
   // run the server and start listen to the port
-  server.listen(process.env.PORT || 3000);
+  server.listen(PORT);
 
 })();
